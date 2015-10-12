d1455a5a82fe9d34a00c8c1826d03db2be05d6bf
jimmyDunne <james.dunne@stanford.edu>
2015-10-12 10:31:23 -0700

    successful unittests using nosetests

--- test_pipeline_2392.py
+++ test_pipeline_2392.py
@@ -11,58 +11,45 @@ import filecmp
 import unittest
 import opensim as osm
 
-path2Model = os.path.join(os.getcwd(), 'models', 'Gait2392_Simbody','gait2392_simbody.osim')
-path2ScaleSetup = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','subject01_Setup_Scale.xml')
-path2IKSetup = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','subject01_Setup_IK.xml')
 path2RRASetup = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','subject01_Setup_RRA.xml')
 path2CMCSetup = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','subject01_Setup_CMC.xml')
+path2ScaleSetup = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','subject01_Setup_Scale.xml')
 
-
-def run_scaling(path2Model,path2ScaleSetup):
-
-	newModelPath = os.path.dirname(path2ScaleSetup) + '/' + nt.basename(path2Model)
-	# copy the model into the new repo
-	sh.copyfile(path2Model, newModelPath)
-	# define the input string to the exe
-	cmd = 'scale -S ' + path2ScaleSetup
-	# run scale
-	os.system(cmd)
-	# Clean up the directory (delete generic model)
-	os.remove(newModelPath)
-	# Get the output model name and return it.
-	scaletool = osm.ScaleTool(path2ScaleSetup)
-	# get the output model name.
-	scaledModelName = scaletool.getMarkerPlacer().getOutputModelFileName()
-	# return the model filename
-	return scaledModelName
+def run_scaling(path2ScaleSetup):
+    # define the input string to the exe
+    cmd = 'scale -S ' + path2ScaleSetup
+    # run scale
+    os.system(cmd)
 
 def run_ik(subjectModel, path2IKSetup):
-	# get and handle to the model
-	model = osm.Model(subjectModel)
-	# make a ikTool object
-	ik = osm.InverseKinematicsTool(path2IKSetup)
-	# set the model on the object
-	ik.setModel(model)
-	# run IK
-	ik.run()
+    # get and handle to the model
+    model = osm.Model(subjectModel)
+    # make a ikTool object
+    ik = osm.InverseKinematicsTool(path2IKSetup)
+    # set the model on the object
+    ik.setModel(model)
+    # run IK
+    ik.run()
+    # return the output file name
+    return ik.getOutputMotionFileName()
 
 def run_rra(path2RRASetup):
-	# setup the cmd command
-	cmd = 'rra -S ' + path2RRASetup
-	# run rra using command line
-	os.system(cmd)
+    # setup the cmd command
+    cmd = 'rra -S ' + path2RRASetup
+    # run rra using command line
+    os.system(cmd)
 
 def run_cmc(path2CMCSetup):
-	# setup the cmd command
-	cmd = 'cmc -S ' + path2CMCSetup
-	# run the cmd command
-	os.system(cmd)
+    # setup the cmd command
+    cmd = 'cmc -S ' + path2CMCSetup
+    # run the cmd command
+    os.system(cmd)
 
 
 # run scaling
-scaledModelName = run_scaling(path2Model,path2ScaleSetup)
+#scaleModelName = run_scaling(path2ScaleSetup)
 # set the full path to the model.
-subjectModel = os.path.dirname(path2ScaleSetup) + '/' + scaledModelName
+#new_Model = os.path.dirname(path2ScaleSetup) + '/' + scaleModelName
 # run IK
 #run_ik(subjectModel, path2IKSetup)
 # run rra
@@ -70,18 +57,38 @@ subjectModel = os.path.dirname(path2ScaleSetup) + '/' + scaledModelName
 # run cmc
 #run_cmc(path2CMCSetup)
 
-ref_Model  = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','OutputReference', 'subject01_simbody.osim')
-ref_motion = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody','OutputReference', 'subject01_walk1_ik.mot')
+global subject_Model
 
+class TestPipelineGait2392(unittest.TestCase):
 
-def test_scaling(ref_Model, subjectModel):
+    # the models and pipelines are in separate folders. Define the file paths.
+    # Then copy the model into the pipeline dir
+    testDir = os.path.join(os.getcwd(), 'pipelines', 'Gait2392_Simbody')
+    path2Model = os.path.join(os.getcwd(), 'models', 'Gait2392_Simbody','gait2392_simbody.osim')
+    path2ScaleSetup = os.path.join(testDir,'subject01_Setup_Scale.xml')
+    path2IKSetup = os.path.join(testDir,'subject01_Setup_IK.xml')
+    # reference files
+    ref_Model  = os.path.join(testDir,'OutputReference', 'subject01_simbody.osim')
+    ref_motion = os.path.join(testDir,'OutputReference', 'subject01_walk1_ik.mot')
 
-	filecmp.cmp(ref_Model, subjectModel)
+    # get the scaled model name
+    scaletool = osm.ScaleTool(path2ScaleSetup)
+    subjectModel = scaletool.getMarkerPlacer().getOutputModelFileName()
 
-	if false
+    def test_scaling(self):
+        # copy the model into the new repo
+        newModelPath = os.path.dirname(self.path2ScaleSetup) + '/' + nt.basename(self.path2Model)
+        sh.copyfile(self.path2Model, newModelPath)
+        # run the OpenSim scale tool
+        run_scaling(self.path2ScaleSetup)
+        # compare the reference model file (xml) with scaled model file.
+        self.assertTrue(filecmp.cmp(self.ref_Model,os.path.join(self.testDir, self.subjectModel)))
 
-#def test_ik():
+    def test_ik(self):
+        # run ik
+        motionName = run_ik(os.path.join(self.testDir,self.subjectModel),self.path2IKSetup )
+        # test the ouput motion with the ref motion.
+        self.assertTrue(filecmp.cmp(os.path.join(self.testDir,motionName),self.ref_motion))
 
-#def test_rra():
-
-#def test_cmc():
+    def test_fake(self):
+        self.assertTrue(filecmp.cmp(self.ref_Model, self.ref_Model))
