72d3a022544265a41ed545efbbcef96f1ad5e1c1
james Dunne <jamesdunne@jamess-MacBook-Pro.local>
2018-11-05 12:58:39 -0500

    Update test

--- test_models.py
+++ test_models.py
@@ -4,8 +4,8 @@
 import os
 import sys
 import unittest
-from fnmatch import fnmatch
-import opensim as om
+fropensim fnmatch import fnmatch
+#import opensim as opensim
 root = os.getcwd()
 pattern = "*.osim"
 
@@ -23,13 +23,29 @@ for i in range(len(osimpaths)):
     # Without this next line, the print above does not necessarily
     # precede OpenSim's output.
     sys.stdout.flush()
+    filename = osimpaths[i]
+    modelname = modelNames[i]
     try:
-        model = om.Model(osimpaths[i])
+        model = opensim.Model(filename)
         s = model.initSystem()
     except Exception as e:
-        print("Oops, Model '%s' failed:\n%s" % (modelNames[i], e.message))
+        print("Oops, Model '%s' failed:\n%s" % (modelname, e.message))
         sys.exit(1)
-        
+
+    # Print the 4.0 version to file and then read back into memory
+    filename_new = filename.replace('.osim','_new.osim')
+    modelName_new = modelName.replace('.osim','_new.osim')
+    # Print the 4.0 model to file
+    model.print(filename_new)
+    # Try and read back in the file
+    try:
+        model = opensim.Model(filename_new)
+        s = model.initSystem()
+    except  Exception as e:
+        print("Oops, 4.0 written Model '%s' failed:\n%s" % (modelName_new, e.message))
+        sys.exit(1)
+
+    # Remove the printed file
+    os.remove(filename_new)
+
 print("All models loaded successfully.")
-
-
