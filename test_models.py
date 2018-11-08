70a599cbcf83ddb0a84322c5df110ad9fd864701
Christopher Dembia <cld72@cornell.edu>
2018-11-08 15:17:58 -0800

    Fix modelName

--- test_models.py
+++ test_models.py
@@ -11,13 +11,13 @@ root = os.getcwd()
 pattern = "*.osim"
 
 osimpaths = list()
-modelNames = list()
+modelnames = list()
 
 for path, subdirs, files in os.walk(root):
     for name in files:
         if fnmatch(name, pattern):
             osimpaths.append(os.path.join(path, name))
-            modelNames.append(name)
+            modelnames.append(name)
 
 for i in range(len(osimpaths)):
     print("\n\n\n" + 80 * "=" + "\nLoading model '%s'" % osimpaths[i] + "\n" + 80 * "-")
@@ -25,7 +25,7 @@ for i in range(len(osimpaths)):
     # precede OpenSim's output.
     sys.stdout.flush()
     filename = osimpaths[i]
-    modelname = modelNames[i]
+    modelname = modelnames[i]
     try:
         model = opensim.Model(filename)
         s = model.initSystem()
@@ -35,7 +35,7 @@ for i in range(len(osimpaths)):
 
     # Print the 4.0 version to file and then read back into memory
     filename_new = filename.replace('.osim','_new.osim')
-    modelName_new = modelName.replace('.osim','_new.osim')
+    modelname_new = modelname.replace('.osim','_new.osim')
     # Print the 4.0 model to file
     model.printToXML(filename_new)
     # Try and read back in the file
@@ -43,14 +43,14 @@ for i in range(len(osimpaths)):
         reloadedModel = opensim.Model(filename_new)
         s2 = reloadedModel.initSystem()
     except  Exception as e:
-        print("Oops, 4.0 written Model '%s' failed:\n%s" % (modelName_new, e.message))
+        print("Oops, 4.0 written Model '%s' failed:\n%s" % (modelname_new, e.message))
         sys.exit(1)
 
     # Remove the printed file
     os.remove(filename_new)
 
     if not reloadedModel.isEqualTo(model):
-        print("Initial instance of '%s' is not equal to :\n%s" % (modelName,modelName_new))
+        print("Initial instance of '%s' is not equal to :\n%s" % (modelname,modelname_new))
         raise Exception("Compared two instances of 4.0 models are not equal")
 
 print("All models loaded successfully.")
